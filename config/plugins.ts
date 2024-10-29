df405f22ff5d59270e64399e909f968b7009821f
felixokpalaima <felix.okpalaima@mynehomes.de>
2024-10-29 14:59:45 +0100

    configure strapi to upload to bucket

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+// export default () => ({});
+export default ({ env }: { env: (key: string) => string }) => ({
+    upload: {
+        config: {
+            provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
+            providerOptions: {
+                bucketName: env('GCS_BUCKET_NAME'),
+                publicFiles: false,
+                uniform: false,
+                basePath: '',
+            },
+        },
+    },
+});
